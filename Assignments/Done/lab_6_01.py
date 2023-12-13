6de00b9a666b39dd0edcffd68b3d8082d4f9f5b5
Junu_Choi_laptop <a01081270127@gmail.com>
2023-12-12 16:00:08 -0800

    lab 6.02

--- Assignments/Done/lab_6_01.py
+++ Assignments/Done/lab_6_01.py
@@ -1,7 +1,10 @@
+'''Add/remove/View pets'''
 #################################################################################
 import os
 #################################################################################3
 class Pet():
+    '''Represents pet'''
+
     def __init__(self,name,type_animal,sound):
         self.name = name
         self.type_animal = type_animal
@@ -9,6 +12,8 @@ class Pet():
 
 
     def add_pet(self):
+        '''This function will add new pet instance'''
+
         #Pet name
         self.name = input("---------------------------\nPet Name: ").lower().capitalize()
 
@@ -37,7 +42,6 @@ class Pet():
                     self.sound = "I AM THE FISH"
                 input("\nSuccessfully Added! \nPress Enter to continue...")
                 return True
-            
             #If pet type user typed is not in pet type list.
             print('''
             Error! We don't take this type of pet.
@@ -50,11 +54,12 @@ Error!
 You cannot leave it empty. 
 
 Please try again
-Press Enter to continue...''')
-            
-        return False
-    
+Press Enter to continue...''') 
+        return False    
     def adopt_pet(self):
+        '''This function will eventually delete current pet instance
+        By using del method.'''
+
         while True:
             os.system("cls")
 
@@ -77,8 +82,9 @@ Pet Name: ''').lower().capitalize()
                 #If pet type user typed is in pet type list.
                 if self.type_animal in pet_type_list:
 
-                    for i in range(1,capacity):
-                        if globals()[f"pet_{i}"].name == self.name:
+                    for i in range(1,CAPACITY):
+                        if globals()[f"pet_{i}"].name == self.name and globals()[f"pet_{i}"].type_animal == self.type_animal:
+                            #Delete current instance
                             del globals()[f"pet_{i}"]
                             input("\nSuccessfully Adopted! \nPress Enter to continue...")
                             return False
@@ -90,50 +96,71 @@ Try again with different name/type!
 Press Enter to continue...''')
                     return True
             #If it's empty string, print Error message
-            input("\nError! You cannot leave it empty. Please try again\nPress Enter to continue...")
+            input('''
+Error! You cannot leave it empty. Please try again
+Press Enter to continue...''')
             return False
 
 def print_pet():
-    for i in range(1,capacity):
+    '''This function will print every name/type/sound of each instances
+    e.g., pet_1, pet_2, pet_3, ...'''
+
+    for i in range(1,CAPACITY):
         try:
+            n = globals()[f"pet_{i}"].name
+            t = globals()[f"pet_{i}"].type_animal
+            s = globals()[f"pet_{i}"].sound
             print(f'''------------------------------
-Name: {globals()[f"pet_{i}"].name}, Type: {globals()[f"pet_{i}"].type_animal}, Sound: {globals()[f"pet_{i}"].sound}''')
-        except:
-            print("\nWe don't have pets currently..")
+Name: {n}, Type: {t}, Sound: {s}''')
+        except KeyError:
+            print("\nAdopted...")
     input("\nPress Enter to continue...")
-    return
 
-def print_detail():
-    pass
+#def print_detail():
+#    pass
 
-capacity = 1
+CAPACITY = 1
+
 pet_type_list = ["cat","dog","turtle","bird","fish"]
 #################################################################################
 while True:
     #Clear Terminal
     os.system("cls")
 
-    #Initializing variables to prevent Errors
+    #Initializing variables to prevent Errors / Constant Variable Naming /
     PETTYPE = ""
     PETNAME = ""
     PETSOUND = ""
 
-    globals()[f"pet_{capacity}"] = Pet(name = PETNAME, type_animal = PETTYPE, sound = PETSOUND)
+    # The globals() method returns a dictionary with all the global 
+    # variables and symbols for the current program.
+    # https://www.programiz.com/python-programming/methods/built-in/globals 
+    #F urther information about "globals()[]" function
+    globals()[f"pet_{CAPACITY}"] = Pet(name = PETNAME, type_animal = PETTYPE, sound = PETSOUND)
 
     #Intro
-    print("-------------------------\n|\tWelcome!\t|\n-------------------------")
+    print('''-------------------------
+|\tWelcome!\t|
+-------------------------''')
 
-    print("|\tAdopt\t\t(a)\n|\tAdd\t\t(b)\n|\tPrint\t\t(c)\n|\tDetail\t\t(d)\n|\texit\t\t(e)\n---------------------------")
+    print('''|\tAdopt\t\t(a)
+|\tAdd\t\t(b)
+|\tPrint\t\t(c)
+|\tDetail\t\t(d)
+|\texit\t\t(e)
+---------------------------''')
     user_input = input("What are you going to do? (a,b,c,d,e): ").strip().lower()
 
     #Adopt (remove animal)
     if user_input == "a":
-        if globals()[f"pet_{capacity}"].adopt_pet():
+        if globals()[f"pet_{CAPACITY}"].adopt_pet():
             continue
+        else:
+            CAPACITY -= 1
     #Add
     elif user_input == "b":
-        if globals()[f"pet_{capacity}"].add_pet():
-            capacity += 1
+        if globals()[f"pet_{CAPACITY}"].add_pet():
+            CAPACITY += 1
         else:
             continue
     #Print
@@ -142,7 +169,7 @@ while True:
     #Detail
     elif user_input == "d":
         input("\nTODO\nPress Enter to continue...")
-        pass #TODO
+        #TODO
     #Exit
     elif user_input == "e":
         input("\nByeBye!\nPress Enter to continue...")
